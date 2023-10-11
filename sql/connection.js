e9acdbd8d1e92d83cb4c4f19c8c94ce53e681154
PeytenBevill <redsage00@gmail.com>
2023-10-10 21:38:16 -0500

    changed database

--- sql/connection.js
+++ sql/connection.js
@@ -1,13 +1,13 @@
 const mysql2 = require('mysql2')
 
 const connection = mysql2.createConnection({
-  host: 'database-1.c400waytzxzf.us-east-1.rds.amazonaws.com',
+  host: 'database-2.c400waytzxzf.us-east-1.rds.amazonaws.com',
   user: 'admin',
-  password: 'Redhead814!',
+  password: 'Inventorypassword',
   database: 'inventorydb',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0 
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0 
 })
 
 module.exports = connection
