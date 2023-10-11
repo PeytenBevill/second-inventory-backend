773216e00f4800c4cd379ca7313f730803ad6794
PeytenBevill <redsage00@gmail.com>
2023-10-10 22:19:34 -0500

    took away additions, starting over

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
 const express = require("express");
-const authenticateJWT = require('./auth')
-const computerRouter = require('./src/routes/R-computers')
-const inventoryRouter = require('./src/routes/R-inventory')
-const cors = require('cors')
+const connection = require("./sql/connection");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
-app.use(cors())
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  // !!THiS IS FOR DEV - We replace this once we have our production URL in place.
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-
-  // res.setHeader(
-  //   "Access-Control-Allow-Origin",
-  //   'https://main--papaya-paprenjak-1f5aa6.netlify.app'
-  // );
-
-  // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.use('/', computerRouter)
-app.use('/', inventoryRouter)
-
+// app.use(cors())
 
 app.get("/", (req, res) => {
   res.json({
@@ -48,17 +14,15 @@ app.get("/", (req, res) => {
   });
 });
 
-
-
-// app.get('/inventory', (req, res) => {
-//   connection.query('SELECT * FROM inventory', (err, results) => {
-//       if(err) {
-//         console.log(err)
-//       } else {
-//         res.json(results)
-//       }
-//   })
-// })
+app.get('/inventory', (req, res) => {
+  connection.query('SELECT * FROM inventory', (err, results) => {
+      if(err) {
+        console.log(err)
+      } else {
+        res.json(results)
+      }
+  })
+})
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
--- sql/connection.js
+++ sql/connection.js
@@ -1,16 +1,13 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql2 = require('mysql2')
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env
 
-const pool = mysql.createPool({
-  connectionLimit: 100,
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-  port: DB_PORT
+
+const connection = mysql2.createPool({
+  host: 'database-2.c400waytzxzf.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Inventorypassword',
+  database: 'inventorydb',
 });
 
 
-module.exports = pool;
+module.exports = connection;
