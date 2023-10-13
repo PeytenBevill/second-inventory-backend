29193dc7c78904137324ae0d02d71d5e7f4a4c9e
PeytenBevill <redsage00@gmail.com>
2023-10-12 19:49:34 -0500

    trying to access production

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://elegant-donut-fc6e45.netlify.app",
+    origin: "https://elegant-donut-fc6e45.netlify.app/",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     optionsSuccessStatus: 200,
